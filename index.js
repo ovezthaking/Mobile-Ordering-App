f122b73355514724d20811b60efb84ff8af4a5f3
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-05 17:54:56 +0100

    Add item removal functionality and update total price calculation

--- index.js
+++ index.js
@@ -3,23 +3,43 @@ import { menuArray } from "./data.js"
 
 document.addEventListener('click', (e) => {
     if (e.target.dataset.addItem) handleAddItemClick(e.target.dataset.addItem)
+    else if (e.target.dataset.removeItem) handleRemoveItemClick(e.target.dataset.removeItem)
 })
 
+let orderItemId = 0
 
 function handleAddItemClick(itemId){
     const item = menuArray.filter(item => item.id == itemId)[0]
 
     const { name, price } = item
+    const currentOrderItemId = orderItemId ++
 
     document.querySelector('.order-products').innerHTML += `
-        <div class="order-product">
-            <h3>${name}<button class="remove-btn" data-removeItem="${itemId}">remove</button></h3>
+        <div class="order-product" data-order-id="${currentOrderItemId}">
+            <h3>${name}<button class="remove-btn" data-remove-item="${itemId}" data-order-id="${currentOrderItemId}">remove</button></h3>
             <h4 class="order-price">$${price}</h4>
         </div>
     `
     const totalPrice = document.getElementById('total-price')
     let totalPriceInt = parseInt(totalPrice.textContent.slice(1))
     totalPriceInt += price
+    
+    totalPrice.textContent = `$${totalPriceInt}`
+}
+
+function handleRemoveItemClick(itemId){
+    console.log('remove', itemId)
+    const removeBtn = event.target
+    const orderItemIdToRemove = removeBtn.dataset.orderId
+    const item = menuArray.filter(item => item.id == itemId)[0]
+    const { price } = item
+    
+    const orderProduct = document.querySelector(`.order-product[data-order-id="${orderItemIdToRemove}"]`)
+    orderProduct.remove()
+
+    const totalPrice = document.getElementById('total-price')
+    let totalPriceInt = parseInt(totalPrice.textContent.slice(1))
+    totalPriceInt -= price
 
     totalPrice.textContent = `$${totalPriceInt}`
 }
