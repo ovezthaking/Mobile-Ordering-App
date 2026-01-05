9b1a72b86d573d4a58f9203e3b681642b2c515ce
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-01-05 17:22:37 +0100

    Total price calculation and products to your order adding

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 import { menuArray } from "./data.js"
 
 
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.addItem) handleAddItemClick(e.target.dataset.addItem)
+})
+
+
+function handleAddItemClick(itemId){
+    const item = menuArray.filter(item => item.id == itemId)[0]
+
+    const { name, price } = item
+
+    document.querySelector('.order-products').innerHTML += `
+        <div class="order-product">
+            <h3>${name}<button class="remove-btn" data-removeItem="${itemId}">remove</button></h3>
+            <h4 class="order-price">$${price}</h4>
+        </div>
+    `
+    const totalPrice = document.getElementById('total-price')
+    let totalPriceInt = parseInt(totalPrice.textContent.slice(1))
+    totalPriceInt += price
+
+    totalPrice.textContent = `$${totalPriceInt}`
+}
+
 function getProductsHtml(){
     const productsHtml = menuArray.map(product => {
         const { name, ingredients, id, price, emoji } = product
@@ -15,7 +38,7 @@ function getProductsHtml(){
                     <p class="ingredients">${ingredientsString}</p>
                     <h3>$${price}</h3>
                 </div>
-                <i class="fa-solid fa-circle-plus icon"></i>
+                <i class="fa-solid fa-circle-plus icon" data-add-item="${id}"></i>
             </section>
         `
     }).join('')
